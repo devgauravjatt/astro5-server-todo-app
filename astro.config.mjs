0d533cc500efc8fc863b0d58fd80d57239adb392
devgauravjatt <devgauravjatt@gmail.com>
2025-01-02 07:06:39 +0530

    netlify add on git

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+	output: 'server',
 
-  experimental: {
-      svg: true,
+	experimental: {
+		svg: true,
 	},
 
-  adapter: netlify(),
+	adapter: netlify(),
 })
